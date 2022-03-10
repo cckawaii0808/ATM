177bc8dfdbcf2eb174e90fb7ccdae6c12125ac82
cc <cc_xie@gmail.com>
2022-03-10 15:28:16 +0800

    可以連線到websocket

--- app/src/main/java/com/ccplay/atm/MainActivity.kt
+++ app/src/main/java/com/ccplay/atm/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ccplay.atm
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -10,9 +11,14 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.ccplay.atm.databinding.ActivityMainBinding
+import okhttp3.*
+import okio.ByteString
+import java.net.URL
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
-
+    private val TAG = MainActivity::class.java.simpleName
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
@@ -28,13 +34,21 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        //webtest()//取得下面網站'
+
+
     }
+
+    //networking
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)//把物件充氣到那個地方
         return true//有布林值」所以要回傳 且９８％為true
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {//當某人在清單選擇試就會來這邊
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -46,6 +60,7 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
--- app/src/main/java/com/ccplay/atm/SecondFragment.kt
+++ app/src/main/java/com/ccplay/atm/SecondFragment.kt
@@ -1,12 +1,21 @@
 package com.ccplay.atm
 
+import android.content.ContentValues
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.ccplay.atm.databinding.FragmentSecondBinding
+import com.google.gson.Gson
+import okhttp3.*
+import okio.ByteString
+import java.net.URL
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,14 +23,15 @@ import com.ccplay.atm.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+    private val TAG = "SecondFragment"
+    lateinit var socket: WebSocket
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
@@ -34,11 +44,78 @@ class SecondFragment : Fragment() {
 
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+
         }
-    }
+        val client = OkHttpClient.Builder().readTimeout(10, TimeUnit.SECONDS)//最多等延遲１０秒
+            .build()
+        val request = Request.Builder()
+            .url("wss://lott-dev.lottcube.asia/ws/chat/chat:app_test?nickname=CC")
+            .build()
+
+
+       socket= client.newWebSocket(request, object : WebSocketListener() {
+            //crtl+o可以複寫//要求寫一個傾聽器 他會自己去處理
+            override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+                super.onClosed(webSocket, code, reason)
+                Log.d(TAG,"onClosed")
+            }
+
+            override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+                super.onClosing(webSocket, code, reason)
+                Log.d(TAG,"onClosing")
+            }
+
+            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                super.onFailure(webSocket, t, response)
+                Log.d(TAG,"onFailure")
+
+            }
+
+            override fun onMessage(webSocket: WebSocket, text: String) {
+                super.onMessage(webSocket, text)
+                Log.d(TAG,"onMessage:=$text")
+            }
+
+            override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+                super.onMessage(webSocket, bytes)
+                Log.d(TAG,"onMessage=${bytes.hex()}")
+            }
+
+            override fun onOpen(webSocket: WebSocket, response: Response) {
+                super.onOpen(webSocket, response)
+                Log.d(TAG,"onOpen")
+            }
+
+        }
+       )
+        binding.sendmessagebotton.setOnClickListener {
+            val message=binding.chatbox.text.toString()
+            val json = "{\"action\": \"N\", \"content\": \"$message\" }"
+            socket.send(json)
+        }
+
+
+
+
+
+
+        }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+    //websocket製作
 }
+data class Message (val id: Long, val name: String)
+
+//private fun webtest() {
+//    thread {//避免main和thread 重複用
+//        val data = URL("https://github.com/cckawaii0808")//通訊協定，位置，目錄//要s要加密的網頁
+//            .readText()
+//        Log.d(TAG, "data:$data")
+//
+//
+//    }
+//}
